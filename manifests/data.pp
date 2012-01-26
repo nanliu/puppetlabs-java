beebf9a58c6da33c8ac5bfdfc067ad4ac42ff5a1
Nan Liu <nan@puppetlabs.com>
2012-01-26 09:36:01 -0500

    Remove complicated tests.

--- manifests/data.pp
+++ manifests/data.pp
@@ -6,9 +6,9 @@ class java::data {
       $java_jdk_package_name = 'jdk'
     }
     'debian': {
-      $java_jre_package_name = 'sun-java6-jre'
-      $java_jdk_package_name = 'sun-java6-jdk'
-      $java_response_file = '/var/local/sun-java6.preseed'
+      $java_jre_package_name  = 'sun-java6-jre'
+      $java_jdk_package_name  = 'sun-java6-jdk'
+      $java_response_file     = '/var/local/sun-java6.preseed'
       $java_response_template = "${module_name}/sun-java6.preseed"
     }
   }
